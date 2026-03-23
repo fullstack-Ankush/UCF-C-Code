3c924f02bdf5cad42929a1d9b0798424b49228f6
Ankush Pal <24cs118@lingayasvidyapeeth.edu.in>
2026-03-23 13:06:25 +0530

    Q5 solved of assignment 32 and finished assignment 32"

--- Assignment 32/solutions.cpp
+++ Assignment 32/solutions.cpp
@@ -33,6 +33,25 @@ class Time{
         }
 };
 
+class Circle{
+
+    float radius;
+    public:
+        void setRadius(float  r){
+            radius = r;
+        }
+        void getRadius(){
+            cout<<radius;
+        }
+        float getPerimeter(){
+            return 2*3.14*radius;
+        }
+        float getArea(){
+            return 3.14*radius*radius;
+        }
+
+};
+
 
 
 
@@ -41,10 +60,16 @@ class Time{
 int main(){
     Complex num1;
     Time t1;
+    Circle c1;
     num1.setReal(7);
     num1.setImaginary(-4);
     num1.showComplex();
     t1.setTime(8,12,44);
     t1.showTime();
+    c1.setRadius(4.1);
+    float r1 = c1.getArea();
+    cout<<r1<<endl;
+    float r2 = c1.getPerimeter();
+    cout<<r2<<endl;
 
 }
